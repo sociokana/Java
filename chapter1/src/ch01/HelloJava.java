2170a750033a0035e2461e94453cf0a9da9aa878
socioholmes <sociokana@gmail.com>
2022-05-02 21:36:02 +0900

    hello world

---
+++ chapter1/src/ch01/HelloJava.java
@@ -0,0 +1,18 @@
+package ch01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		int hoge1, hoge2; 
+		hoge1 = 10;
+		hoge2 = 15; 
+		
+		int hoge3 = 20; 
+		
+		System.out.println("hoge1: " + hoge1);
+		System.out.println("hoge2: " + hoge2);
+		System.out.println("hoge3: " + hoge3);
+		
+	}
+
+}
---
+++ chapter1/src/ch01/vars.java
@@ -0,0 +1,23 @@
+package ch01;
+
+public class vars {
+
+	public static void main(String[] args) {
+		int hoge1, hoge2; 
+		hoge1 = 10; 
+		hoge2 = 15; 
+		
+		int hoge3 = 20; 
+		
+		int hogeBin = 0b1010; 
+		int hogeOct = 01010; 
+		int hogeHex = 0x1010; 
+		
+		System.out.println("hoge1: " + hoge1);
+		System.out.println("hoge2: " + hoge2);
+		System.out.println("hoge3: " + hoge3);
+		System.out.println("Bin: " + hogeBin);
+		System.out.println("Oct: " + hogeOct);
+		System.out.println("Hex: " + hogeHex);
+	}	
+}
